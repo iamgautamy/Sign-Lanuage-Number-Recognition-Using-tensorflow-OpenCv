b8522cfee5b6a14e2a73f0ae1bb9d68925c6d8fb
Gautam Yadav <gautamybk7@gmail.com>
2022-03-16 20:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ def video_feed():
     return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True,use_reloader=False)
+    app.run(host='0.0.0.0',debug=True,use_reloader=False)
